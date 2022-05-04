420aa02b5c2d2dbc2977d116e4298d91e86053eb
Mhd <homamsy64@gmail.com>
2022-05-04 16:23:40 +0300

    first

---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+import Cart from "./Cart";
+import { useStateContext } from "../context/stateContext";
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">3BDULBAKI</Link>
+      </p>
+      <button type="button" className="cart-icon" onClick={setShowCart}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Layout } from "../components/";
+import { StateContext } from "../context/stateContext";
+import { Toaster } from "react-hot-toast";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
+}
+
+export default MyApp;
